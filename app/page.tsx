83db388e4f8bddb5fc7e7bbd0390ccc92b6c05a0
Yerovy Espitia <luyeesgar@gmail.com>
2025-04-18 16:38:48 -0500

    Refactor: use PokemonGame and update styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,13 @@
-import { Card } from '@/components/Card'
+import { PokemonGame } from '@/components/PokemonGame'
+import { getPokemon } from '@/utils/get'
 
 export default async function Home() {
-  async function getPokemon() {
-    const randomPokemon = Math.floor(Math.random() * 150) + 1
-    const url = `https://pokeapi.co/api/v2/pokemon/${randomPokemon}`
-    const response = await fetch(url)
-    const data = await response.json()
-    return data
-  }
-
   const pokemon = await getPokemon()
 
-  console.log('pokemon:', pokemon)
-
   return (
-    <main className='bg-red-800 h-screen flex justify-center items-center mx-auto flex-col'>
-      <h1 className='text-white text-5xl font-bold'>Who's that Pokémon?</h1>
-      <Card pokemon={pokemon} />
+    <main className='bg-purple-800 h-screen flex justify-center items-center mx-auto flex-col'>
+      <h1 className='text-white text-5xl font-bold mb-8'>Who's that Pokémon?</h1>
+      <PokemonGame initialPokemon={pokemon} />
     </main>
   )
 }
