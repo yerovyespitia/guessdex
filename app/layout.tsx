db090f2c59c34542030d4fa2dfbea46a450e3811
Yerovy Espitia <luyeesgar@gmail.com>
2025-04-19 18:14:55 -0500

    feat: wrap layout with SocketProvider for socket context management

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Navbar } from '@/components/Navbar'
+import { SocketProvider } from '@/contexts/SocketContext'
 import './globals.css'
 
 const geistSans = Geist({
@@ -28,8 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        {children}
+        <SocketProvider>
+          <Navbar />
+          {children}
+        </SocketProvider>
       </body>
     </html>
   )
