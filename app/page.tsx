1978a0daffd085fadc0c45ceef59ac4d389f3117
Yerovy Espitia <luyeesgar@gmail.com>
2025-04-18 19:38:16 -0500

    style: adjust heading size for improved responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,9 @@ export default async function Home() {
 
   return (
     <main className='bg-purple-800 h-screen flex justify-center items-center mx-auto flex-col'>
-      <h1 className='text-white text-5xl font-bold mb-8'>Who's that Pokémon?</h1>
+      <h1 className='text-white text-3xl md:text-5xl font-bold mb-8'>
+        Who's that Pokémon?
+      </h1>
       <PokemonGame initialPokemon={pokemon} />
     </main>
   )
