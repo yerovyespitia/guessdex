280b8b5e916ca1c21b260c7c8ba7ad4a2eea8bdb
Yerovy Espitia <luyeesgar@gmail.com>
2025-04-19 15:48:35 -0500

    style: adjust main container height and update heading font properties

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default async function Home() {
   const pokemon = await getPokemon()
 
   return (
-    <main className='bg-purple-800 h-screen flex justify-center items-center mx-auto flex-col'>
-      <h1 className='text-white text-3xl md:text-5xl font-bold mb-8'>
+    <main className='bg-purple-800 h-[calc(100vh-64px)] flex justify-center items-center mx-auto flex-col'>
+      <h1 className='text-white text-3xl md:text-5xl font-bold mb-8 font-[Helvetica] tracking-tight'>
         Who's that Pokémon?
       </h1>
       <PokemonGame initialPokemon={pokemon} />
