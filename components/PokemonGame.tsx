43ed9dcf6fb21c43e73c20e45c6b410b09aa337c
Yerovy Espitia <luyeesgar@gmail.com>
2025-04-18 19:39:16 -0500

    refactor: integrate useCountdown hook for improved countdown management and simplify guessing logic

--- components/PokemonGame.tsx
+++ components/PokemonGame.tsx
@@ -1,8 +1,11 @@
 'use client'
-import { useState, useEffect } from 'react'
+
+import { useState } from 'react'
 import { Card } from './Card'
 import { Pokemon } from '@/types/pokemon'
 import { getPokemon } from '@/utils/get'
+import { useCountdown } from '@/hooks/useCountdown'
+import { Countdown } from './Countdown'
 
 type PokemonGameProps = {
   initialPokemon: Pokemon
@@ -10,46 +13,22 @@ type PokemonGameProps = {
 
 export const PokemonGame = ({ initialPokemon }: PokemonGameProps) => {
   const [pokemon, setPokemon] = useState<Pokemon>(initialPokemon)
-  const [countdown, setCountdown] = useState<number | null>(null)
 
   const loadNewPokemon = async () => {
     const newPokemon = await getPokemon()
     setPokemon(newPokemon)
   }
 
-  useEffect(() => {
-    let timer: NodeJS.Timeout
-    if (countdown !== null && countdown > 0) {
-      timer = setTimeout(() => {
-        setCountdown(countdown - 1)
-      }, 1000)
-    } else if (countdown === 0) {
-      loadNewPokemon()
-      setCountdown(null)
-    }
-    return () => clearTimeout(timer)
-  }, [countdown])
-
-  const handleCorrectGuess = () => {
-    setCountdown(3)
-  }
-
-  const handleWrongGuess = () => {
-    loadNewPokemon()
-  }
+  const { countdown, start: startCountdown } = useCountdown(3, loadNewPokemon)
 
   return (
     <div className='flex flex-col items-center'>
       <Card
         pokemon={pokemon}
-        onCorrectGuess={handleCorrectGuess}
-        onWrongGuess={handleWrongGuess}
+        onCorrectGuess={() => startCountdown()}
+        onWrongGuess={() => startCountdown()}
       />
-      <div className='text-white text-2xl font-bold mt-4 h-[36px]'>
-        {countdown !== null && (
-          <p>Next Pokémon in: {countdown}</p>
-        )}
-      </div>
+      <Countdown value={countdown} />
     </div>
   )
 }
