b58d9362aa743967c96fecdb687edc62775325df
Yerovy Espitia <luyeesgar@gmail.com>
2025-04-19 22:56:26 -0500

    refactor: simplify loading and error handling by using Loading and Error

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { Error } from '@/components/Error'
+import { Loading } from '@/components/Loading'
 import { PokemonGame } from '@/components/PokemonGame'
 import { usePokemon } from '@/hooks/usePokemon'
 
@@ -7,28 +9,16 @@ export default function Home() {
   const { data: pokemon, isLoading, error } = usePokemon()
 
   if (isLoading) {
-    return (
-      <main className='bg-purple-800 h-[calc(100vh-64px)] flex justify-center items-center mx-auto flex-col'>
-        <h1 className='text-white text-3xl md:text-5xl font-bold mb-8 font-[Helvetica] tracking-tight'>
-          Loading...
-        </h1>
-      </main>
-    )
+    return <Loading />
   }
 
   if (error) {
-    return (
-      <main className='bg-purple-800 h-[calc(100vh-64px)] flex justify-center items-center mx-auto flex-col'>
-        <h1 className='text-white text-3xl md:text-5xl font-bold mb-8 font-[Helvetica] tracking-tight'>
-          Error loading Pokemon
-        </h1>
-      </main>
-    )
+    return <Error />
   }
 
   return (
     <main className='bg-purple-800 h-[calc(100vh-64px)] flex justify-center items-center mx-auto flex-col'>
-      <h1 className='text-white text-3xl md:text-5xl font-bold mb-8 font-[Helvetica] tracking-tight'>
+      <h1 className='text-white text-3xl md:text-5xl font-bold mb-4 md:mb-8 font-[Helvetica] tracking-tight'>
         Who's that Pokémon?
       </h1>
       {pokemon && <PokemonGame initialPokemon={pokemon} />}
