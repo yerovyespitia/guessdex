72cbcbf8e60be994b8fe22ea776575711e801f37
Yerovy Espitia <luyeesgar@gmail.com>
2025-04-20 15:03:30 -0500

    fix: update SVG path for improved icon representation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -65,7 +65,7 @@ export const Navbar = () => {
                 strokeLinecap='round'
                 strokeLinejoin='round'
                 strokeWidth={2}
-                d='M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z'
+                d='M8 3v18m0-18h8a2 2 0 012 2v14a2 2 0 01-2 2H8m4-9h4'
               />
             </svg>
           </div>
