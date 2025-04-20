f8db9311e85029463e6d3c2a98934b4cecc4124c
Yerovy Espitia <luyeesgar@gmail.com>
2025-04-19 22:58:17 -0500

    style: align items to the right

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,30 +2,30 @@ import Link from 'next/link'
 
 export const Navbar = () => {
   return (
-    <nav className='bg-purple-800 flex text-center justify-between items-center w-full p-4'>
-      <Link
-        href='/'
-        className='flex items-center gap-2 text-white text-md hover:opacity-80 cursor-pointer'
-        title='GuessDex'
-      >
-        <div className='bg-black/20 p-2 rounded-full'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            className='size-4'
-            fill='none'
-            viewBox='0 0 24 24'
-            stroke='currentColor'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              strokeWidth={2}
-              d='M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6'
-            />
-          </svg>
-        </div>
-      </Link>
+    <nav className='bg-purple-800 flex text-center justify-end items-center w-full p-4'>
       <div className='flex gap-4'>
+        <Link
+          href='/'
+          className='flex items-center gap-2 text-white text-md hover:opacity-80 cursor-pointer'
+          title='GuessDex'
+        >
+          <div className='bg-black/20 p-2 rounded-full'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              className='size-4'
+              fill='none'
+              viewBox='0 0 24 24'
+              stroke='currentColor'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth={2}
+                d='M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6'
+              />
+            </svg>
+          </div>
+        </Link>
         <Link
           href='/create-room'
           className='flex items-center gap-2 text-white text-md hover:opacity-80 cursor-pointer'
