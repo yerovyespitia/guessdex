180df602df15b86715e73bc75450703c94481382
Yerovy Espitia <luyeesgar@gmail.com>
2025-04-19 20:28:17 -0500

    fix: update fetch options in getPokemon function to disable caching

--- utils/get.ts
+++ utils/get.ts
@@ -1,7 +1,10 @@
 export async function getPokemon() {
   const randomPokemon = Math.floor(Math.random() * 150) + 1
   const url = `https://pokeapi.co/api/v2/pokemon/${randomPokemon}`
-  const response = await fetch(url)
+  const response = await fetch(url, {
+    cache: 'no-store',
+    next: { revalidate: 0 }
+  })
   const data = await response.json()
   return data
 }
