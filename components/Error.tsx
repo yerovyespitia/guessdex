f44965aef27e3d86f3275a84807a4b576b8c10ed
Yerovy Espitia <luyeesgar@gmail.com>
2025-04-19 22:57:15 -0500

    feat: add Error

---
+++ components/Error.tsx
@@ -0,0 +1,9 @@
+export const Error = () => {
+  return (
+    <main className='bg-purple-800 h-[calc(100vh-64px)] flex justify-center items-center mx-auto flex-col'>
+      <h1 className='text-white text-3xl md:text-5xl font-bold mb-8 font-[Helvetica] tracking-tight'>
+        Error loading Pokemon
+      </h1>
+    </main>
+  )
+}
