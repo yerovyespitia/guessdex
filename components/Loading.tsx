7290a8c0001ceef614ede145a913d837221d7c22
Yerovy Espitia <luyeesgar@gmail.com>
2025-04-19 22:57:21 -0500

    feat: add Loading

---
+++ components/Loading.tsx
@@ -0,0 +1,9 @@
+export const Loading = () => {
+  return (
+    <main className='bg-purple-800 h-[calc(100vh-64px)] flex justify-center items-center mx-auto flex-col'>
+      <h1 className='text-white text-3xl md:text-5xl font-bold mb-8 font-[Helvetica] tracking-tight'>
+        Loading...
+      </h1>
+    </main>
+  )
+}
