b7eaaf37a5ba80608b7dfbd7fb8399ec410fec75
Yerovy Espitia <luyeesgar@gmail.com>
2025-04-21 11:40:03 -0500

    feat: add GuessingStatus

---
+++ components/GuessingStatus.tsx
@@ -0,0 +1,24 @@
+type Props = {
+  isMyTurn: boolean
+  isMultiplayer: boolean
+  players: string[]
+  activePlayerId?: string
+}
+
+export const GuessingStatus = ({
+  isMyTurn,
+  isMultiplayer,
+  players,
+  activePlayerId,
+}: Props) => {
+  if (!isMultiplayer || isMyTurn) return null
+
+  const playerLabel =
+    players.find((p) => p === activePlayerId)?.slice(0, 4) ?? '...'
+
+  return (
+    <div className='mt-4 text-xl font-semibold text-yellow-300'>
+      Waiting for {playerLabel} to guess...
+    </div>
+  )
+}
