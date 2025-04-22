e0ee64e919b35ec31a34649a004e4668ab09efb2
Yerovy Espitia <luyeesgar@gmail.com>
2025-04-22 14:52:47 -0500

    feat: enhance with dynamic error message and navigation button

--- components/Error.tsx
+++ components/Error.tsx
@@ -1,9 +1,23 @@
-export const Error = () => {
+import { useRouter } from 'next/navigation'
+
+type ErrorProps = {
+  error: string
+}
+
+export const Error = ({ error }: ErrorProps) => {
+  const router = useRouter()
+
   return (
-    <main className='bg-purple-800 h-[calc(100vh-64px)] flex justify-center items-center mx-auto flex-col'>
-      <h1 className='text-white text-3xl md:text-5xl font-bold mb-8 font-[Helvetica] tracking-tight'>
-        Error loading Pokemon
+    <div className='bg-purple-800 h-[calc(100vh-64px)] flex justify-center items-center mx-auto flex-col'>
+      <h1 className='text-white text-3xl md:text-4xl font-bold mb-8 font-[Helvetica] tracking-tight'>
+        Error: <span className='font-medium'>{error}</span>
       </h1>
-    </main>
+      <button
+        onClick={() => router.push('/')}
+        className='bg-white font-bold text-purple-800 px-8 py-3 rounded-lg cursor-pointer'
+      >
+        Go Home
+      </button>
+    </div>
   )
 }
