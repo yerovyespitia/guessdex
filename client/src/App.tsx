e5d9af4562f5c64ba96f6a08ded3b003af552ed2
Yerovy Espitia <luyeesgar@gmail.com>
2025-04-23 17:12:28 -0500

    style: decrease header font size

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <main className='bg-purple-800 h-[calc(100vh-64px)] flex justify-center items-center mx-auto flex-col'>
-      <h1 className='text-white text-4xl md:text-5xl font-bold mb-4 md:mb-8 font-[Helvetica] tracking-tight'>
+      <h1 className='text-white text-3xl md:text-5xl font-bold mb-4 md:mb-8 font-[Helvetica] tracking-tight'>
         Who's that Pokémon?
       </h1>
       {pokemon && <PokemonGame initialPokemon={pokemon} />}
