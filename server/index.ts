aaf280143df73a071bab90ba05f489ce2f564350
Yerovy Espitia <luyeesgar@gmail.com>
2025-04-24 19:29:32 -0500

    feat: implement static file serving in HTTP server

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,27 @@
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
 import { setupSocketEvents } from './manage/game'
+import { join } from 'node:path'
+import { existsSync } from 'node:fs'
+import { createReadStream } from 'node:fs'
 
 const PORT = process.env.PORT || 3000
+const httpServer = createServer(async (req, res) => {
+  const filePath = join(
+    import.meta.dir,
+    '../client/dist',
+    req.url === '/' ? 'index.html' : req.url || ''
+  )
 
-console.log('🚀 HTTP + WebSocket server running on PORT ', PORT)
-
-// Create a standard HTTP server
-const httpServer = createServer()
+  if (existsSync(filePath)) {
+    const fileStream = createReadStream(filePath)
+    res.writeHead(200)
+    fileStream.pipe(res)
+  } else {
+    res.writeHead(404)
+    res.end('Not found')
+  }
+})
 
 const io = new Server(httpServer, {
   cors: {
@@ -15,18 +29,15 @@ const io = new Server(httpServer, {
   },
 })
 
-httpServer.listen(PORT, () => {
-  console.log('Socket.IO server running')
-})
-
-// Event Socket.IO
 io.on('connection', (socket) => {
   console.log('🧩 Client connected:', socket.id)
-
-  // Setup all game-related socket events
   setupSocketEvents(io, socket)
 
   socket.on('disconnect', () => {
     console.log('❌ Client disconnected:', socket.id)
   })
+})
+
+httpServer.listen(PORT, () => {
+  console.log(`🚀 HTTP + WebSocket server running on PORT ${PORT}`)
 })
