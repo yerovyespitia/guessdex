a95a707151d6585b74cdc2f888c1ac131b2ca249
Yerovy Espitia <luyeesgar@gmail.com>
2025-04-25 15:20:48 -0500

    style: update button text color and font weight

--- client/src/components/CreateForm.tsx
+++ client/src/components/CreateForm.tsx
@@ -44,7 +44,7 @@ export const CreateForm = ({ disabled }: CreateFormProps) => {
   return (
     <button
       onClick={handleCreateRoom}
-      className='px-6 py-3 w-80 md:w-96 text-lg text-center bg-white rounded-lg text-black outline-none disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer'
+      className='px-6 py-3 w-80 md:w-96 text-lg text-center bg-white rounded-lg text-sky-800 font-semibold outline-none disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer'
       disabled={disabled || !isConnected}
     >
       {isConnected ? 'Create New Room' : 'Connecting...'}
