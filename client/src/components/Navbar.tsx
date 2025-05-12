81ad88ac032fa7044d1ec74a1d00fc3c62b38f56
Yerovy Espitia <luyeesgar@gmail.com>
2025-05-12 07:45:01 -0500

    feat: add mute toggle button to Navbar with sound state management

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { Link } from '@tanstack/react-router'
-
+import { useSoundStore } from '../store/sound'
 export const Navbar = () => {
+  const { isMuted, toggleMute } = useSoundStore()
+
   const navLinks = [
     { to: '/', title: 'GuessDex', icon: 'home' },
     { to: '/create-room', title: 'Create Room', icon: 'plus' },
@@ -10,6 +12,16 @@ export const Navbar = () => {
   return (
     <nav className='bg-sky-800 flex text-center justify-end items-center w-full p-4'>
       <div className='flex gap-4'>
+        <button
+          onClick={toggleMute}
+          className='bg-black/20 p-3 rounded-full hover:opacity-80 cursor-pointer'
+        >
+          <img
+            src={`/svgs/${isMuted ? 'muted' : 'volume'}.svg`}
+            alt='volume'
+            className='filter invert'
+          />
+        </button>
         {navLinks.map(({ to, title, icon }) => (
           <Link
             key={to}
