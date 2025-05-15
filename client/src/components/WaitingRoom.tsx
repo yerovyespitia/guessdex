5ea44c04268c3c9e32bb5d31fbb301f49181748a
Yerovy Espitia <luyeesgar@gmail.com>
2025-05-15 09:31:53 -0500

    refactor: update to accept title prop and improve layout

--- client/src/components/WaitingRoom.tsx
+++ client/src/components/WaitingRoom.tsx
@@ -21,28 +21,51 @@ const gameModes = [
   },
 ]
 
-const generations = [
-  { value: 'all', label: 'All Generations' },
-  { value: 'gen1', label: 'Gen 1 (Kanto)' },
-  { value: 'gen2', label: 'Gen 2 (Johto)' },
-  { value: 'gen3', label: 'Gen 3 (Hoenn)' },
-  { value: 'gen4', label: 'Gen 4 (Sinnoh)' },
-  { value: 'gen5', label: 'Gen 5 (Unova)' },
-  { value: 'gen6', label: 'Gen 6 (Kalos)' },
-  { value: 'gen7', label: 'Gen 7 (Alola)' },
-  { value: 'gen8', label: 'Gen 8 (Galar)' },
-]
+// const generations = [
+//   { value: 'all', label: 'All Generations' },
+//   { value: 'gen1', label: 'Gen 1 (Kanto)' },
+//   { value: 'gen2', label: 'Gen 2 (Johto)' },
+//   { value: 'gen3', label: 'Gen 3 (Hoenn)' },
+//   { value: 'gen4', label: 'Gen 4 (Sinnoh)' },
+//   { value: 'gen5', label: 'Gen 5 (Unova)' },
+//   { value: 'gen6', label: 'Gen 6 (Kalos)' },
+//   { value: 'gen7', label: 'Gen 7 (Alola)' },
+//   { value: 'gen8', label: 'Gen 8 (Galar)' },
+// ]
 
-export const WaitingRoom = () => {
+export const WaitingRoom = ({ title }: { title: string }) => {
   return (
-    <div className='flex flex-col items-center justify-center bg-white text-white w-full px-12 py-8 rounded-lg max-w-5xl mx-auto'>
-      <h1 className='text-3xl font-bold mb-2 text-zinc-800'>Waiting Room</h1>
-      <p className='font-light text-zinc-500'>
-        Choose your game settings while waiting for other players
-      </p>
+    <div className='flex flex-col justify-center bg-white text-white w-full px-6 py-8 rounded-lg max-w-5xl mx-auto'>
+      <section className='flex flex-col items-center gap-2 mb-4'>
+        <h1 className='text-2xl md:text-3xl font-bold text-zinc-800'>
+          {title}
+        </h1>
+        <p className='font-light text-zinc-500'>
+          Choose your game settings for the game
+        </p>
+      </section>
+      {/* <section className='flex flex-col gap-2 mb-4'>
+        <label
+          className='font-semibold text-lg md:text-xl text-zinc-800'
+          htmlFor='nickname'
+        >
+          Your Nickname
+        </label>
+        <input
+          type='text'
+          id='nickname'
+          placeholder='Enter your nickname'
+          className='w-full px-4 py-2 bg-white text-black rounded-lg border-2 border-zinc-200 hover:border-zinc-300 focus:outline-none focus:ring-2 focus:ring-white'
+          maxLength={15}
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+        />
+      </section> */}
       <div className='flex flex-col gap-2'>
-        <h2 className='font-semibold text-xl text-zinc-800'>Game mode</h2>
-        <div className='flex gap-2 mb-4'>
+        <h2 className='font-semibold text-lg md:text-xl text-zinc-800'>
+          Game modes
+        </h2>
+        <div className='flex flex-col md:flex-row gap-2 mb-4'>
           {gameModes.map((mode) => (
             <label
               key={mode.id}
@@ -62,9 +85,9 @@ export const WaitingRoom = () => {
             </label>
           ))}
         </div>
-        <div className='flex flex-col gap-2 mb-4'>
+        {/* <div className='flex flex-col gap-2 mb-4'>
           <label
-            className='font-semibold text-xl text-zinc-800'
+            className='font-semibold text-lg md:text-xl text-zinc-800'
             htmlFor='generation'
           >
             Pokémon Generation
@@ -83,8 +106,8 @@ export const WaitingRoom = () => {
               </option>
             ))}
           </select>
-        </div>
-        <div className='flex flex-col gap-2 mb-4'>
+        </div> */}
+        {/* <div className='flex flex-col gap-2 mb-4'>
           <div className='flex gap-2'>
             <label className='relative inline-flex items-center cursor-pointer'>
               <input
@@ -97,8 +120,8 @@ export const WaitingRoom = () => {
               </span>
             </label>
           </div>
-        </div>
-        <div className='flex flex-col gap-2 bg-zinc-100 p-4 rounded-lg'>
+        </div> */}
+        {/* <div className='flex flex-col gap-2 bg-zinc-100 p-4 rounded-lg'>
           <div className='flex flex-col gap-2'>
             <div className='flex items-center justify-between gap-2'>
               <div className='flex items-center gap-2'>
@@ -120,7 +143,7 @@ export const WaitingRoom = () => {
               <div className='w-full h-2 bg-gray-300 rounded-full animate-pulse'></div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   )
