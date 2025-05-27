f7eee95325040028b06b98201576e8f375e64d1b
Yerovy Espitia <luyeesgar@gmail.com>
2025-05-27 13:03:29 -0500

    fix: adjust layout for settings route to improve responsiveness

--- client/src/routes/settings.tsx
+++ client/src/routes/settings.tsx
@@ -8,7 +8,7 @@ export const Route = createFileRoute('/settings')({
 function RouteComponent() {
   return (
     <main className='bg-sky-800 h-[calc(100vh-76px)] flex justify-center items-center mx-auto flex-col'>
-      <div className='flex flex-col gap-4 items-center mx-4'>
+      <div className='flex flex-col gap-4 items-center mx-4 w-full px-4 md:w-auto'>
         <WaitingRoom title='Game Settings' />
       </div>
     </main>
